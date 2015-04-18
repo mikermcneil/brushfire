7f97cd65f8f2e6605bb227fc1fa972ec6a241230
Irl Nathan <irlnathan@gmail.com>
2015-04-17 21:49:39 -0500

    chapter4

---
+++ api/controllers/VideoController.js
@@ -0,0 +1,11 @@
+/**
+ * VideoController
+ *
+ * @description :: Server-side logic for managing videos
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
--- assets/js/videos-page.js
+++ assets/js/videos-page.js
@@ -1,26 +1,14 @@
-/**
- * Some JavaScript for our page with the interactive list of videos, using Angular for the wiring.
- *
- * At the end of the day, the sLOC (number of lines of source code excluding comments and such)
- * is 44 for the jQuery version, and 43 for the Angular version.  In other words, the difference is
- * negligible.  Your front-end JavaScript framework is a personal/business decision, and there are
- * advantages and disadvantages to every approach you might take.  Whether it's Ember, React, Backbone,
- * Knockout, a native iOS app, a native Android app, or an army of robot ants, Sails is accepting of
- * your choice(s).  As you'll see in subsequent chapters, this front-end agnosticism is a key design
- * pattern of modern web apps, and something you should always keep in the back of your mind.
- */
-
 angular.module('brushfire_videosPage', [])
-.config(function($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    'self',
-    '*://www.youtube.com/**'
-  ]);
-});
+  .config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      '*://www.youtube.com/**'
+    ]);
+  });
 
 angular.module('brushfire_videosPage').controller('PageCtrl', [
-            '$scope', '$timeout',
-  function ( $scope ,  $timeout ){
+  '$scope', '$http',
+  function($scope, $http) {
 
     /////////////////////////////////////////////////////////////////////////////
     // Immediately start fetching list of videos from the server.
@@ -29,33 +17,28 @@ angular.module('brushfire_videosPage').controller('PageCtrl', [
     // First, show a loading spinner
     $scope.videosLoading = true;
 
-    // Then simulate a delay
-    // (TODO: actually fetch videos from server instead of pretending)
-    $timeout(function afterRetrievingVideos (){
+    $scope.submitVideosError = false;
 
-      // TODO: handle error state from the server
 
-      // Fake data (TODO: use the real data from the server instead of pretending)
-      var _videos = [{
-        title: 'PSY - GANGNAM STYLE (강남스타일) M/V',
-        src: 'https://www.youtube.com/embed/9bZkp7q19f0'
-      }, {
-        title: 'Justin Bieber - Baby ft. Ludacris',
-        src: 'https://www.youtube.com/embed/kffacxfA7G4'
-      }, {
-        title: 'Charlie bit my finger - again !',
-        src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
-      }];
-
-      // Hide the loading spinner:
+    // Get the existing videos.
+    io.socket.get('/video', function whenServerResponds(data, JWR) {
       $scope.videosLoading = false;
 
-      // Stick the videos into the DOM
-      $scope.videos = _videos;
+      if (JWR.statusCode >= 400) {
+       $scope.submitVideosError = true;
+        console.log('something bad happened');
+        return;
+      }
 
-    }, 750);
+      $scope.videos = data;
 
-    // };
+      // Apply the changes to the DOM
+      // (we have to do this since `io.socket.get` is not a
+      // angular-specific magical promisy-thing)
+      $scope.$apply();
+
+    });
+
 
     ///////////////////////////////////////////////////////////////
     // SET UP LISTENERS FOR DOM EVENTS
@@ -66,7 +49,7 @@ angular.module('brushfire_videosPage').controller('PageCtrl', [
      * (the binding from our form's "submit" event to this function is
      *  handled via `ng-submit="submitNewVideo($event)` in the HTML)
      */
-    $scope.submitNewVideo = function (){
+    $scope.submitNewVideo = function() {
 
       // A little "spin-lock" to prevent double-submission
       // (because disabling the submit button still allows double-posts
@@ -82,18 +65,18 @@ angular.module('brushfire_videosPage').controller('PageCtrl', [
         src: $scope.newVideoSrc,
       };
 
-    	// create placeholder anchor element
-    	var parser = document.createElement('a');
+      // create placeholder anchor element
+      var parser = document.createElement('a');
 
-    	// assign url to parser.href
-    	parser.href = _newVideo.src
+      // assign url to parser.href
+      parser.href = _newVideo.src
 
-    	// Use the indexOf parser.search as the first argument and length of
-    	// parser.search as the second argument of parser.search.substring
-    	// The result is the YouTube ID --> LfOWehvvuo0
-    	var youtubeID = parser.search.substring(parser.search.indexOf("=")+1, parser.search.length);
+      // Use the indexOf parser.search as the first argument and length of
+      // parser.search as the second argument of parser.search.substring
+      // The result is the YouTube ID --> LfOWehvvuo0
+      var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1, parser.search.length);
 
-      _newVideo.src = 'https://www.youtube.com/embed/'+youtubeID;
+      _newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
 
       // (this is where you put your client-side validation when relevant)
 
@@ -115,25 +98,49 @@ angular.module('brushfire_videosPage').controller('PageCtrl', [
       // (also disables form submission)
       $scope.busySubmittingVideo = true;
 
-      // Simulate a delay
-      $timeout(function (){
-        // TODO: handle error state from the server
-        // Success!
-        // Now we know it's the real deal and the server accepted our submission.
+      io.socket.post('/video', {
+        title: _newVideo.title,
+        src: _newVideo.src
+      }, function whenServerResponds(data, JWR) {
 
-        // Insert HTML for the newly added video into the DOM
+        $scope.videosLoading = false;
+
+        if (JWR.statusCode>=400) {
+          console.log('something bad happened');
+          return;
+        }
+
         $scope.videos.unshift(_newVideo);
 
         // Hide the loading state
         // (also re-enables form submission)
         $scope.busySubmittingVideo = false;
 
-        // Clear out form inputs
+        //Clear out form inputs
         $scope.newVideoTitle = '';
         $scope.newVideoSrc = '';
 
-      }, 750);
+        $scope.$apply();
+
+      });
+
+      io.socket.on('video', function whenAVideoIsCreatedUpdatedOrDestroyed(event) {
+
+        // Add the new video to the DOM
+        $scope.videos.unshift({
+          title: event.data.title,
+          src: event.data.src,
+
+        });
+
+        // Apply the changes to the DOM
+        // (we have to do this since `io.socket.get` is not a
+        // angular-specific magical promisy-thing)
+        $scope.$apply();
+      });
+
 
     };
+
   }
 ]);
