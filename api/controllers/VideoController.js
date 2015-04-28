90a808b1b5e456992921e3ade0f59613f64d49d2
Irl Nathan <irlnathan@gmail.com>
2015-04-27 23:15:10 -0500

    adding the searchVideos machine

--- api/controllers/VideoController.js
+++ api/controllers/VideoController.js
@@ -8,6 +8,23 @@
 module.exports = {
 
   find: function(req, res) {
+    var Youtube = require('machinepack-youtube');
+
+    // List Youtube videos which match the specified search query.
+    Youtube.searchVideos({
+      query: 'grumpy cat',
+      apiKey: 'xAmBxAmBxAmBkjbyKkjbyKkjbyK',
+      limit: 15,
+    }).exec({
+      // An unexpected error occurred.
+      error: function(err) {
+
+      },
+      // OK.
+      success: function(result) {
+
+      },
+    });
 
     return res.json([{
       title: 'Cat eats dog.',
