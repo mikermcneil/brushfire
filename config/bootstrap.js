0b7ed6e5b50bf67b48924632d56c563f630e0a7a
Irl Nathan <irlnathan@gmail.com>
2015-05-08 23:26:54 -0500

    clean-up for end of chapter 5

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,38 +11,8 @@
 
 module.exports.bootstrap = function(cb) {
 
-  var Youtube = require('machinepack-youtube');
-
-  // Display the number of views of a Youtube Video.
-  Youtube.getPlayCount({
-    url: 'https://www.youtube.com/watch?v=TruIq5IxuiU',
-    apiKey: 'AIzaSyCT2IHOJJDgoTxAmBkjbyKu_xU8LY_sA64',
-  }).exec({
-
-    // An unexpected error occurred.
-    error: function(err) {
-
-      // Respond with a simple error message.
-      return cb(err);
-    },
-    // OK.
-    success: function(videos) {
-
-      // Marshal the video data from machinepack-youtube
-      // to look just like the stuff in our database.
-      _.each(videos, function(video){
-        video.title = video.name;
-        delete video.title;
-      });
-
-      // TODO: store these videos in our database
-      console.log(videos);
-
-      // It's very important to trigger this callback method when you are finished
-      // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-      return cb();
-    
-    }
-  });
+  // It's very important to trigger this callback method when you are finished
+  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  return cb();
 
 };
