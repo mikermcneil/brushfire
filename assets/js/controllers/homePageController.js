b0dd45c6f5615c46b9879d00411dcb2210d5d7f0
Irl Nathan <irlnathan@gmail.com>
2015-07-08 14:23:31 -0500

    chap7-move submitLoginForm() method from homePageController to navPageController

--- assets/js/controllers/homePageController.js
+++ assets/js/controllers/homePageController.js
@@ -7,51 +7,51 @@ angular.module('brushfire').controller('homePageController', ['$location', '$sco
 
   $scope.me = window.SAILS_LOCALS.me;
 
-  $scope.submitLoginForm = function() {
+  // $scope.submitLoginForm = function() {
 
-    // Set the loading state (i.e. show loading spinner)
-    $scope.loginForm.loading = true;
+  //   // Set the loading state (i.e. show loading spinner)
+  //   $scope.loginForm.loading = true;
 
-    // Submit request to Sails.
-    $http.put('/login', {
-        email: $scope.loginForm.email,
-        password: $scope.loginForm.password
-      })
-      .then(function onSuccess() {
-        // Refresh the page now that we've been logged in.
-        window.location = '/videos';
-      })
-      .catch(function onError(sailsResponse) {
+  //   // Submit request to Sails.
+  //   $http.put('/login', {
+  //       email: $scope.loginForm.email,
+  //       password: $scope.loginForm.password
+  //     })
+  //     .then(function onSuccess() {
+  //       // Refresh the page now that we've been logged in.
+  //       window.location = '/videos';
+  //     })
+  //     .catch(function onError(sailsResponse) {
 
-        if (sailsResponse.status === 403) {
-          // toastr.error('Removed', 'Error', {
-          //   closeButton: true
-          // });
-          window.location = '/restore';
-          return;
-        }
+  //       if (sailsResponse.status === 403) {
+  //         // toastr.error('Removed', 'Error', {
+  //         //   closeButton: true
+  //         // });
+  //         window.location = '/restore';
+  //         return;
+  //       }
 
-        // Handle known error type(s).
-        // Invalid username / password combination.
-        if (sailsResponse.status === 400 || 404) {
-          // $scope.loginForm.topLevelErrorMessage = 'Invalid email/password combination.';
-          //
-          toastr.error('Invalid email/password combination.', 'Error', {
-            closeButton: true
-          });
-          return;
-        }
+  //       // Handle known error type(s).
+  //       // Invalid username / password combination.
+  //       if (sailsResponse.status === 400 || 404) {
+  //         // $scope.loginForm.topLevelErrorMessage = 'Invalid email/password combination.';
+  //         //
+  //         toastr.error('Invalid email/password combination.', 'Error', {
+  //           closeButton: true
+  //         });
+  //         return;
+  //       }
 
-        toastr.error('An unexpected error occurred, please try again.', 'Error', {
-          closeButton: true
-        });
-        return;
+  //       toastr.error('An unexpected error occurred, please try again.', 'Error', {
+  //         closeButton: true
+  //       });
+  //       return;
 
-      })
-      .finally(function eitherWay() {
-        $scope.loginForm.loading = false;
-      });
-  };
+  //     })
+  //     .finally(function eitherWay() {
+  //       $scope.loginForm.loading = false;
+  //     });
+  // };
 
 
 }]);
--- assets/js/controllers/navPageController.js
+++ assets/js/controllers/navPageController.js
@@ -1,5 +1,56 @@
 angular.module('brushfire').controller('navPageController', ['$location', '$scope', '$http', 'toastr', function($location, $scope, $http, toastr) {
 
+  //Set-up loading state
+  $scope.loginForm = {};
+
+  // Assign the window object to the scope
   $scope.me = window.SAILS_LOCALS.me;
+
+  $scope.submitLoginForm = function() {
+
+    // Set the loading state (i.e. show loading spinner)
+    $scope.loginForm.loading = true;
+
+    // Submit request to Sails.
+    $http.put('/login', {
+        email: $scope.loginForm.email,
+        password: $scope.loginForm.password
+      })
+      .then(function onSuccess() {
+        // Redierct the page now that we've been logged in.
+        // window.location = '/videos';
+        toastr.success('We have a match!', 'Success', {closeButton: true});
+      })
+      .catch(function onError(sailsResponse) {
+
+        // if (sailsResponse.status === 403) {
+        //   // toastr.error('Removed', 'Error', {
+        //   //   closeButton: true
+        //   // });
+        //   window.location = '/restore';
+        //   return;
+        // }
+
+        // Handle known error type(s).
+        // Invalid username / password combination.
+        if (sailsResponse.status === 400 || 404) {
+          // $scope.loginForm.topLevelErrorMessage = 'Invalid email/password combination.';
+          //
+          toastr.error('Invalid email/password combination.', 'Error', {
+            closeButton: true
+          });
+          return;
+        }
+
+        toastr.error('An unexpected error occurred, please try again.', 'Error', {
+          closeButton: true
+        });
+        return;
+
+      })
+      .finally(function eitherWay() {
+        $scope.loginForm.loading = false;
+      });
+  };
 
 }]);
