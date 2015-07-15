d054a3e71a1d3766c66a85f08113fedd569d52bc
Irl Nathan <irlnathan@gmail.com>
2015-07-15 17:53:17 -0500

    showProfilePage route

--- config/routes.js
+++ config/routes.js
@@ -68,16 +68,16 @@ module.exports.routes = {
   //     }
   //   }
   // },
-  'GET /profile': {
-    view: 'profile',
-    locals: {
-      me: {
-        id: 1,
-        gravatarURL: 'http://www.gravatar.com/avatar/ef3eac6c71fdf24b13db12d8ff8d1264?',
-        email: 'sailsinaction@gmail.com'
-      }
-    }
-  },
+  // 'GET /profile': {
+  //   view: 'profile',
+  //   locals: {
+  //     me: {
+  //       id: 1,
+  //       gravatarURL: 'http://www.gravatar.com/avatar/ef3eac6c71fdf24b13db12d8ff8d1264?',
+  //       email: 'sailsinaction@gmail.com'
+  //     }
+  //   }
+  // },
   'GET /edit-profile': {
     view: 'edit-profile',
     locals: {
